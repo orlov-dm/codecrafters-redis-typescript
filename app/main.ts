e9b064d410f25e692ad753792ff71482cc05fba6
Dmitrii Orlov <dm.ed.orlov@gmail.com>
2025-01-19 17:35:02 +0200

    upd: Refactor for Multiple client usage

--- app/main.ts
+++ app/main.ts
@@ -3,14 +3,17 @@ import * as net from "net";
 const PING_CMD = 'ping';
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
-  connection.addListener('data', (data) => {
+  connection.on('data', (data) => {
     const input = data.toString().toLowerCase();    
     let index = input.indexOf(PING_CMD);
     while (index !== -1) {      
       connection.write('+PONG\r\n');
       index = input.indexOf(PING_CMD, index + PING_CMD.length);
-    }
+    }    
   })
+  connection.on("close", () => {
+    connection.end();
+  });
 });
 
 server.listen(6379, "127.0.0.1");
