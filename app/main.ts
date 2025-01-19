117b552baa30f922ddd01949708beaf93248887f
Dmitrii Orlov <dm.ed.orlov@gmail.com>
2025-01-19 15:20:49 +0200

    feat: PING test

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,16 @@
 import * as net from "net";
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
-  // Handle connection
+  connection.addListener('data', (data) => {
+    const command = data.toString();
+    console.log('command', command);
+    switch (command) {
+      default:
+        connection.write('+PONG\r\n');
+    }
+    connection.pipe(connection);
+    connection.end();
+  })
 });
 
 server.listen(6379, "127.0.0.1");
