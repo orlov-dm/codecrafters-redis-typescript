cb3bcba28b3fa4ded95cbea4506484ccf39aa62c
Dmitrii Orlov <dm.ed.orlov@gmail.com>
2025-01-19 16:26:27 +0200

    feat: Multiple PING from the same connection

--- app/main.ts
+++ app/main.ts
@@ -1,15 +1,15 @@
 import * as net from "net";
 
+const PING_CMD = 'ping';
+
 const server: net.Server = net.createServer((connection: net.Socket) => {
   connection.addListener('data', (data) => {
-    const command = data.toString();
-    console.log('command', command);
-    switch (command) {
-      default:
-        connection.write('+PONG\r\n');
+    const input = data.toString().toLowerCase();    
+    let index = input.indexOf(PING_CMD);
+    while (index !== -1) {      
+      connection.write('+PONG\r\n');
+      index = input.indexOf(PING_CMD, index + PING_CMD.length);
     }
-    connection.pipe(connection);
-    connection.end();
   })
 });
 
